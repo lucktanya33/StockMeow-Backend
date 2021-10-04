38aa4677bef30f8c34ec9ef3e303302cfa7203af
Tanya <Tanya@TanyadeMacBook-Air.local>
2021-10-04 12:28:15 +0800

    set local origin as allowed origin for cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3001
 
 app.use(express.json());
 app.use(cors({
-  origin: true,
+  origin: 'http://localhost:3000',
   methods: ["GET", "POST"],
   credentials: true
 }))
