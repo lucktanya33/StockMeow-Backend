9dcf3c483b18e6db67122dfe8317778163381675
Tanya <Tanya@TanyadeMacBook-Air.local>
2021-10-04 12:39:08 +0800

    trying fix CORS by middleware function

--- index.js
+++ index.js
@@ -8,11 +8,20 @@ const cookieParser = require("cookie-parser")
 const port = process.env.PORT || 3001
 
 app.use(express.json());
-app.use(cors({
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+})
+
+/*app.use(cors({
   origin: 'http://localhost:3000',
   methods: ["GET", "POST"],
   credentials: true
-}))
+}))*/
 
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
