b8ec3fa3f726786dfd614cca2e6a2b92f3ec4eb8
Tanya <Tanya@TanyadeMacBook-Air.local>
2021-10-06 00:06:28 +0800

    reopen server

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.post('/register', (req, res, next) => {
   [username, password], 
   (err, result) => {
     if (err) {
-      console.log('hi');
+      console.log('hii');
       if (err.code === 'PROTOCOL_CONNECTION_LOST') {
         // db error 重新連線
         disconnect_handler();
